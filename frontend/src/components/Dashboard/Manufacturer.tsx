52dc4b6f4d8e26f5e726c5a6bc7afdfe262eec6a
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-06-02 13:56:46 +0530

    filtering and pagination functionality added

--- frontend/src/components/Dashboard/Manufacturer.tsx
+++ frontend/src/components/Dashboard/Manufacturer.tsx
@@ -1,46 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/store";
-import '../../scss/auth.scss'
+import "../../scss/auth.scss";
 import { IUserRegister } from "../../types/user.types";
 import { getMessageApi, postMessageApi } from "../../utils/api";
 import { IMessages } from "../../types/messages.types";
+import { toast } from "react-toastify";
 
-const Manufacturer: React.FC<{token:string}> = ({token}) => {
+interface ManufacturerProps {
+  token: string;
+}
+
+const Manufacturer: React.FC<ManufacturerProps> = ({ token }) => {
   const dispatch = useDispatch<any>();
   const [to, setTo] = useState("");
   const [from, setFrom] = useState("");
-  const [user , setUser] = useState<any>({})
+  const [user, setUser] = useState<any>({});
   const [quantity, setQuantity] = useState(1);
-  const [load , setLoad] = useState(false)
+  const [load, setLoad] = useState(false);
   const [transporter, setTransporter] = useState("");
-  const [allUsers , setAllUsers] = useState<IUserRegister[]>([])
-  const { messages } = useSelector(
-    (state: RootState) => state.messages
-  );
+  const [allUsers, setAllUsers] = useState<IUserRegister[]>([]);
+  const { messages } = useSelector((state: RootState) => state.messages);
+  const [searchOrderID, setSearchOrderID] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(4);
+
   useEffect(() => {
     const curUser = localStorage.getItem("user");
     const parsedUser = curUser ? JSON.parse(curUser) : {};
-    setUser(parsedUser)
-  },[])
-
-
-  
+    setUser(parsedUser);
+  }, []);
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_REACT_URL}/user`).then((res) => res.json()).then((res) => setAllUsers(res.allUsers)).catch((err) => console.log(err));
+    fetch(`${import.meta.env.VITE_REACT_URL}/user`)
+      .then((res) => res.json())
+      .then((res) => setAllUsers(res.allUsers))
+      .catch((err) => console.log(err));
   }, []);
 
   const handleSendMessage = async () => {
-    const messageData : IMessages = {
+    const messageData: IMessages = {
       to,
       from,
       quantity,
       transporter,
     };
-    setLoad(true)
-    await dispatch(postMessageApi(messageData , token))
-    setLoad(false)
+    setLoad(true);
+    await dispatch(postMessageApi(messageData, token));
+    setLoad(false);
+    toast.success("Message Added")
     setTo("");
     setFrom("");
     setQuantity(1);
@@ -48,11 +56,27 @@ const Manufacturer: React.FC<{token:string}> = ({token}) => {
   };
 
   useEffect(() => {
-    dispatch(getMessageApi(token))
-  },[messages])
+    dispatch(getMessageApi(token));
+  }, [messages]);
 
-  const filteredUser = allUsers?.filter((el) => el.role !== "Manufacturer")
-  const filteredMessages = messages?.filter((el : IMessages) => el.userID === user?._id)
+  const filteredUser = allUsers?.filter((el) => el.role !== "Manufacturer");
+  const filteredMessages = messages?.filter(
+    (el: IMessages) =>
+      el.userID === user?._id &&
+      el.orderID?.toLowerCase().includes(searchOrderID.toLowerCase()) // Filter by orderID
+  );
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const paginatedMessages = filteredMessages?.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="manufacture">
       <form>
@@ -81,7 +105,6 @@ const Manufacturer: React.FC<{token:string}> = ({token}) => {
             value={quantity}
             onChange={(e) => setQuantity(Number(e.target.value))}
           >
-            
             <option value="1">1 ton</option>
             <option value="2">2 tons</option>
             <option value="3">3 tons</option>
@@ -96,37 +119,84 @@ const Manufacturer: React.FC<{token:string}> = ({token}) => {
           >
             <option value="">Select transporter</option>
             {filteredUser?.map((el) => (
-              <option key={el.username} value={el.username}>{el.username}</option>
+              <option key={el.username} value={el.username}>
+                {el.username}
+              </option>
             ))}
           </select>
         </div>
-        <button type="button" onClick={handleSendMessage} className={load ? "loading" : ""}>
-        {load ? "Loading.." : "Send Message"}
+        <button
+          type="button"
+          onClick={handleSendMessage}
+          className={load ? "loading" : ""}
+        >
+          {load ? "Loading.." : "Send Message"}
         </button>
       </form>
       <div className="messages">
-      <h3>Messages:</h3>
-      {filteredMessages?.length > 0 ? (
-        <div className="message-container">
-          {filteredMessages.map((message) => (
-            <div className="message-row" key={message.orderID}>
-              <p><span>Order ID: </span> {message.orderID}</p>
-              <p><span>To: </span> {message.to}</p>
-              <p><span>From: </span> {message.from}</p>
-              <p><span>Quantity: </span> {message.quantity}</p>
-              <p><span>Address: </span> {message.address}</p>
-              <p><span>Transporter: </span> {message.transporter}</p>
-              <p><span>Payment: </span>{message.sent ? `Done (${message.price}$)` : "Pending"}</p>
-            </div>
-          ))}
-           
+        <h3>Messages:</h3>
+        <div className="search-by-orderID">
+          <label htmlFor="orderID">Search by Order ID:</label>
+          <input
+            type="text"
+            id="orderID"
+            value={searchOrderID}
+            onChange={(e) => setSearchOrderID(e.target.value)}
+          />
         </div>
-      ) : (
-        <p>No Messages Found</p>
-      )}
+        {paginatedMessages?.length > 0 ? (
+          <div className="message-container">
+            {paginatedMessages.map((message) => (
+              <div className="message-row" key={message.orderID}>
+                <p>
+                  <span>Order ID: </span> {message.orderID}
+                </p>
+                <p>
+                  <span>To: </span> {message.to}
+                </p>
+                <p>
+                  <span>From: </span> {message.from}
+                </p>
+                <p>
+                  <span>Quantity: </span> {message.quantity}
+                </p>
+                <p>
+                  <span>Address: </span> {message.address}
+                </p>
+                <p>
+                  <span>Transporter: </span> {message.transporter}
+                </p>
+                <p>
+                  <span>Payment: </span>
+                  {message.sent ? `Done (${message.price}$)` : "Pending"}
+                </p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No Messages Found</p>
+        )}
+        <div className="pagination">
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </button>
+          <span>{currentPage}</span>
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={indexOfLastItem >= filteredMessages?.length}
+          >
+            {" "}
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Manufacturer
+export default Manufacturer;
+
+
--- frontend/src/components/Dashboard/Transporter.tsx
+++ frontend/src/components/Dashboard/Transporter.tsx
@@ -1,16 +1,19 @@
-import { useEffect, useState , FC } from "react";
+
+import { useEffect, useState, FC } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import { getMessageApi, paymentMessageApi } from "../../utils/api";
 import { IMessages } from "../../types/messages.types";
 import { toast } from "react-toastify";
 
-const Transporter: FC<{token:string}> = ({token}) => {
-  const [amount , setAmount] = useState(0);
-  const [load , setLoad] = useState(false)
-  const { messages } = useSelector(
-    (state: RootState) => state.messages
-  );
+const Transporter: FC<{ token: string }> = ({ token }) => {
+  const [amount, setAmount] = useState(0);
+  const [load, setLoad] = useState(false);
+  const [searchOrderID, setSearchOrderID] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [messagesPerPage] = useState(4);
+
+  const { messages } = useSelector((state: RootState) => state.messages);
 
   const dispatch = useDispatch<any>();
 
@@ -25,53 +28,101 @@ const Transporter: FC<{token:string}> = ({token}) => {
     (el: IMessages) => el.transporter === parsedUser.username
   );
 
+  // Search messages by Order ID
+  const filteredMessages = filterMessages?.filter((el) =>
+    el.orderID?.toLowerCase().includes(searchOrderID.toLowerCase())
+  );
+
+  // Pagination
+  const indexOfLastMessage = currentPage * messagesPerPage;
+  const indexOfFirstMessage = indexOfLastMessage - messagesPerPage;
+  const currentMessages = filteredMessages?.slice(
+    indexOfFirstMessage,
+    indexOfLastMessage
+  );
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   const handlePayment = async (id: any) => {
-    console.log(id)
-    setLoad(true)
-    await dispatch(paymentMessageApi(amount, id , token));
-    setLoad(false)
-    toast.success("Payment done Successfully")
+    setLoad(true);
+    await dispatch(paymentMessageApi(amount, id, token));
+    setLoad(false);
+    toast.success("Payment done Successfully");
   };
 
   return (
     <div className="transporter">
       <div className="messages">
+        <div className="search-by-orderID">
+          <label htmlFor="searchOrderID">Search by Order ID:</label>
+          <input
+            type="text"
+            id="searchOrderID"
+            value={searchOrderID}
+            onChange={(e) => setSearchOrderID(e.target.value)}
+          />
+        </div>
         <h2>Messages:</h2>
-        {filterMessages?.length > 0 ? <div className="message-container">
-          {filterMessages?.map((el) => (
-            <div className="message-row" key={el._id}>
-              <p>
-                <span>Order ID:</span> {el.orderID}
-              </p>
-              <p>
-                <span>To:</span> {el.to}
-              </p>
-              <p>
-                <span>From:</span> {el.from}
-              </p>
-              <p>
-                <span>Quantity:</span> {el.quantity}
-              </p>
-              <p>
-                <span>Address:</span> {el.address}
-              </p>
-              {el.sent ? (
-                <div>Payment Done ✔</div>
-              ) : (
-                <div>
-                  <input
-                    type="number"
-                    placeholder="payment amount"
-                    onChange={(e) => setAmount(Number(e.target.value))}
-                  />
-                  <button onClick={() => handlePayment(el._id)} className={load ? "loading" : ""}>{load ? "Wait" : "Send"}</button>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>:
-        <p>No Messages Found</p>
-        }
+        {currentMessages?.length > 0 ? (
+          <div className="message-container">
+            {currentMessages?.map((el) => (
+              <div className="message-row" key={el._id}>
+                <p>
+                  <span>Order ID:</span> {el.orderID}
+                </p>
+                <p>
+                  <span>To:</span> {el.to}
+                </p>
+                <p>
+                  <span>From:</span> {el.from}
+                </p>
+                <p>
+                  <span>Quantity:</span> {el.quantity}
+                </p>
+                <p>
+                  <span>Address:</span> {el.address}
+                </p>
+                {el.sent ? (
+                  <div>Payment Done ✔</div>
+                ) : (
+                  <div>
+                    <input
+                      type="number"
+                      placeholder="payment amount"
+                      onChange={(e) => setAmount(Number(e.target.value))}
+                    />
+                    <button
+                      onClick={() => handlePayment(el._id)}
+                      className={load ? "loading" : ""}
+                    >
+                      {load ? "Wait" : "Send"}
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No Messages Found</p>
+        )}
+        {/* Pagination */}
+        {filteredMessages && filteredMessages.length > messagesPerPage && (
+          <div className="pagination">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => paginate(currentPage - 1)}
+            >
+              Previous
+            </button>
+            <span>{currentPage}</span>
+            <button
+              disabled={indexOfLastMessage >= filteredMessages.length}
+              onClick={() => paginate(currentPage + 1)}
+            >
+              Next
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
